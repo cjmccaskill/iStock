3b693b0521bbc90d17d865267c18c468ad3867be
CJ McCaskill <cjmccaskill@gmail.com>
2021-07-27 21:33:06 -0500

    redirect the route to home

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Stock from "./pages/Stock";
 import Dashboard from "./pages/Dashboard";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
         <Route path="/home">
           <Home />
         </Route>
